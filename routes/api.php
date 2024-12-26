890e7a65a611eaffcfe053f258326f0dbcb84bc1
Ananya B R <ananyaballal4@gmail.com>
2024-12-26 15:42:44 +0530

    update the api.php file for login

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
-<<<<<<< HEAD
-use App\Http\Controllers\Auth\LoginController; // Include LoginController
-=======
-use App\Http\Controllers\LoginController;
->>>>>>> f3dd1813f1d1718939807a78ed42c3698735d6b4
 use App\Http\Controllers\StudentDashboardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,23 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Protected route (requires token authentication)
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/retrieve-profile-picture', [StudentDashboardController::class, 'retrieveProfilePicture']);
-Route::post('/retrieve-pan-card', [StudentDashboardController::class, 'panCardView']);
-Route::post('/retrieve-aadhar-card', [StudentDashboardController::class, 'aadharCardView']);
-Route::post('/loginformdata', [LoginController::class, 'loginFormData'])->name('loginformdata');
-Route::post('/count-documents', [StudentDashboardController::class, 'countFilesInBucket']);
-Route::post('/from-profileupdate', [StudentDashboardController::class, 'updateFromProfile']);
 
-// User Registration
-Route::post('/registeruser', [RegisterController::class, 'register']);
-Route::get('/registeruser', [RegisterController::class, 'showMessage']);
+// Route::post('/registeruser', [RegisterController::class, 'register']);
 
-// User Login
-Route::post('/loginformdata', [LoginController::class, 'loginFormData'])->name('loginformdata');
+// Route::get('/registeruser', [RegisterController::class, 'showMessage']);
 
-// Example of a Student Dashboard route (uncomment and modify if needed)
-// Route::get('/dashboard', [StudentDashboardController::class, 'index']);
+//Route::post('/loginformdata', [LoginController::class, 'loginFormData'])->name('loginformdata');
+
+
