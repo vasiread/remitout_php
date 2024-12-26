7848d4b439550cb639cd237e1ac775b14d64e055
Ananya B R <ananyaballal4@gmail.com>
2024-12-26 17:04:45 +0530

    update the api.php file for login

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,28 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\StudentDashboardController;
+use App\Http\Controllers\Auth\LoginController;  // Ensure LoginController is imported
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+|--------------------------------------------------------------------------|
+| API Routes                                                              |
+|--------------------------------------------------------------------------|
+| Here is where you can register API routes for your application. These    |
+| routes are loaded by the RouteServiceProvider within a group which       |
+| is assigned the "api" middleware group. Enjoy building your API!         |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json($request->user());
 });
 
+// User registration route
 Route::post('/registeruser', [RegisterController::class, 'register']);
+
+// Optionally, show a message (this can be removed if not needed)
 Route::get('/registeruser', [RegisterController::class, 'showMessage']);
+
+// Login route (ensure LoginController is imported)
 Route::post('/loginformdata', [LoginController::class, 'loginFormData'])->name('loginformdata');
-
-
