56f86ddd14f013e1e8b02bbd9bece9cb114a5670
Ananya B R <ananyaballal4@gmail.com>
2024-12-26 17:04:45 +0530

    update the api.php file for login

--- routes/api.php
+++ routes/api.php
@@ -10,24 +10,22 @@ use App\Http\Controllers\OTPMobController;
 use App\Http\Controllers\scDashboardController;
 use App\Http\Controllers\SidebarHandlingController;
 use App\Http\Controllers\StudentCounsellorController;
-use App\Http\Controllers\StudentDashboardController;
+use App\Http\Controllers\Auth\LoginController;  // Ensure LoginController is imported
 use App\Http\Controllers\TrackController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+|--------------------------------------------------------------------------|
+| API Routes                                                              |
+|--------------------------------------------------------------------------|
+| Here is where you can register API routes for your application. These    |
+| routes are loaded by the RouteServiceProvider within a group which       |
+| is assigned the "api" middleware group. Enjoy building your API!         |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json($request->user());
 });
 Route::post('/retrieve-profile-picture', [StudentDashboardController::class, 'retrieveProfilePicture']);
 Route::post('/retrieve-pan-card', [StudentDashboardController::class, 'panCardView']);
