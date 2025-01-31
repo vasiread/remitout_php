edafe270ea233163d8b68840a920ee53f17043fb
vasiread <vasirajaread@gmail.com>
2025-01-31 12:59:42 +0530

    sc-dashboard 95% frontend

--- app/Http/Controllers/SidebarHandlingController.php
+++ app/Http/Controllers/SidebarHandlingController.php
@@ -14,7 +14,23 @@ class SidebarHandlingController extends Controller
             ['name' => 'My Applications', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
         ];
 
-         return view('pages.scdashboard', compact('sidebarItems'));
+        return view('pages.scdashboard', compact('sidebarItems'));
+    }
+    public function admindashboardItems()
+    {
+        $sidebarItems = [
+            ['name' => 'Dashboard', 'icon' => 'fa-solid fa-square-poll-vertical', 'active' => true],
+            ['name' => 'Inbox', 'icon' => 'fa-solid fa-inbox', 'active' => false],
+            ['name' => 'Student', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+            ['name' => 'Student Counsellor', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+            ['name' => 'NBFC', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+            ['name' => 'Manage Student', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+            ['name' => 'Role Management', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+            ['name' => 'Edit Content', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+            ['name' => 'Promotional', 'icon' => 'fa-regular fa-clipboard', 'active' => false],
+        ];
+
+        return view('pages.scdashboard', compact('sidebarItems'));
     }
 
 }
