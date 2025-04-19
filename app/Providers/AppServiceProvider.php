1830f7f82dce7440acde11cd687c3ea522f40aba
ananyaballal <ananyaballalb@gmail.com>
2025-04-19 11:24:10 +0530

    fix the issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,14 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+     public function boot()
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+        // if (env('APP_ENV') !== 'production') {
+        //     URL::forceScheme('https');
+        // }
         //
     }
 }
