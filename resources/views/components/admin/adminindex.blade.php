a3be3c7b191d9d21c5c5ac56cdfa8ac204596e07
ananyaballal <ananyaballalb@gmail.com>
2025-04-26 15:49:13 +0530

    Fix admin student management Report and its functionality

--- resources/views/components/admin/adminindex.blade.php
+++ resources/views/components/admin/adminindex.blade.php
@@ -7,9 +7,6 @@
     <link rel="stylesheet" href="assets/css/nbfc.css">
     <link rel="stylesheet" href="assets/css/adminindex.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <style>
-        
-    </style>
 </head>
 <body>
     @extends('layouts.app')
